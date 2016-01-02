5cc261f42fd5f75394861c7c02e1457b1dbd7917
Saran <saran.rk@gmail.com>
2016-01-01 20:47:39 -0700

    minor change

--- Zanzibar.RenameFiles/MainWindow.xaml.cs
+++ Zanzibar.RenameFiles/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -55,7 +56,7 @@ namespace Zanzibar.RenameFiles
 
         private void cmdRename_Click(object sender, RoutedEventArgs e)
         {
-            RenameFiles(txtDirectoryName.Text);
+            RenameFilesv1(txtDirectoryName.Text);
             System.Windows.MessageBox.Show("Renamed Successfully!");
         }
 
@@ -95,5 +96,58 @@ namespace Zanzibar.RenameFiles
                 }
             }
         }
+
+        private void RenameFilesv1(string directoryName)
+        {
+
+            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+            DirectoryInfo d = new DirectoryInfo(directoryName);
+            FileSystemInfo[] infos = d.GetFileSystemInfos();
+
+
+            foreach (FileSystemInfo f in infos)
+            {
+                if (!(f.Name.ToUpper().Equals("THUMBS.DB")))
+                {
+                    FileAttributes attr = File.GetAttributes(f.FullName);
+                    if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                        RenameFilesv1(f.FullName);
+                    // Files in directory
+                    else
+                    {
+                        //FileInfo fileInfo = new FileInfo(f.FullName);
+                        //var fileName = fileInfo.Name;
+
+                        TagLib.File ft = TagLib.File.Create(f.FullName);
+                        ft.Tag.Title = System.IO.Path.GetFileNameWithoutExtension(f.FullName);
+                        ft.Tag.AlbumArtists = new string[] { "K. J. Yesudas" };
+                        ft.Tag.Performers= new string[] { "K. J. Yesudas" };
+                        ft.Tag.Album = "K. J. Yesudas Song";
+                        ft.Tag.Track = 0;
+                        ft.Save();
+
+                        //var fileNumber = fileName.Substring(0,4);
+                        //if (IsNumber(fileNumber))
+                        //{
+                        //    var namewithOutExtension = System.IO.Path.GetFileNameWithoutExtension(fileInfo.FullName);
+                        //    var ext = System.IO.Path.GetExtension(fileInfo.FullName);
+                        //    var fileNameWithOutNumber = namewithOutExtension.Substring(4);
+
+                        //    var newFileName = fileInfo.DirectoryName + "\\" + myTI.ToTitleCase(fileNameWithOutNumber.ToLower())+".mp3";
+                        //    File.Move(fileInfo.FullName, newFileName);
+                        //}
+                        //else
+                        //{
+                        //    var namewithOutExtension = System.IO.Path.GetFileNameWithoutExtension(fileInfo.FullName);
+                        //    var ext = System.IO.Path.GetExtension(fileInfo.FullName);
+
+                        //    var newFileName = fileInfo.DirectoryName + "\\" + "001 " + myTI.ToTitleCase(namewithOutExtension.ToLower().Replace("_"," ")) + ".mp3";
+                        //    File.Move(fileInfo.FullName, newFileName);
+                        //}
+                    }
+                }
+            }
+        }
+
     }
 }
